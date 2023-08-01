e7c90eca086a08ea41306f1d1fb19d1b268acc51
Zoya <ezoya@yahoo.com>
2023-08-01 11:34:47 +0100

    Initial Commit

---
+++ UI3/UI3App.swift
@@ -0,0 +1,17 @@
+//
+//  UI3App.swift
+//  UI3
+//
+//  Created by scholar on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct UI3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
